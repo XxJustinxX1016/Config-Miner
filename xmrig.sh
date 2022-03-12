599f4405d99d37c64b8fd227ee90c0b7278defaf
XxJustinxX1016 <98905045+XxJustinxX1016@users.noreply.github.com>
2022-03-12 05:45:02 +0000

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+set e
+
+################################################
+#
+#                     Config
+#                  Miner
+#                     Xmrig
+#                  Latest
+#
+################################################
+
+
+script="v0.1"
+
+
+print_brake() {
+  for ((n = 0; n < $1; n++)); do
+    echo -n "#"
+  done
+  echo ""
+}
+
+hyperlink() {
+  echo -e "\e]8;;${1}\a${1}\e]8;;\a"
+}
+
+#### Colors ####
+
+reset="\e[0m"
+red='\033[0;31m'
+
+error() {
+  red='\033[0;31m'
+  reset="\e[0m"
+
+  echo ""
+  echo -e "* ${red}ERROR${reset}: $1"
+  echo ""
+}
+
+
+#### Check Sudo ####
+
+if [[ $EUID -ne 0 ]]; then
+  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  exit 1
+fi
+
+
+#### Check Curl ####
+
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+cancel() {
+echo
+echo -e "* ${red}Installation Canceled!${reset}"
+done=true
+exit 1
+}
+
+done=false
+
+echo
+print_brake 70
+echo "* Xmrig-Config @ $script"
+print_brake 70
+echo
+
+Xmrig() {
+bash <(curl https://raw.githubusercontent.com/XxJustinxX1016/Config-Miner/main/xmrig.sh)
+}
+
+
+while [ "$done" == false ]; do
+  options=(
+    "Xmrig (Xmrig)"
+    
+    
+        "Cancel Installation"
+  )
+
+
+  actions=(
+    "Xmrig"
+    
+    "cancel"
+  )
+  
+  echo "* Xmrig - Config"
+  echo
+  
+  for i in "${!options[@]}"; do
+    echo "[$i] ${options[$i]}"
+  done
+  
+  echo
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+  
+  [ -z "$action" ] && error "Input is required" && continue
+  
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && eval "${actions[$action]}"
